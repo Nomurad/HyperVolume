f2b16d4244278a48e1cdb7504e1c106f14f6598a
seiya nomura <nomurad@outlook.jp>
2019-12-19 18:39:18 +0900

    this code can calc hv(2dim).

--- src/hypervolume.py
+++ src/hypervolume.py
@@ -1,19 +1,173 @@
 import numpy as np 
+import matplotlib.pyplot as plt
+
+class Individual(object):
+    def __init__(self, dv_list:list, obj_list):
+        self.dv = np.array(dv_list)
+        self.obj = np.array(obj_list)
+        self.n_dv = len(self.dv)
+        self.n_obj = len(self.obj)
+
+    #selfがotherを支配する場合 -> True
+    def dominate(self, other)-> bool:
+        if not isinstance(other, Individual):
+            Exception("not indiv.")
+        
+        if all( s <= o for s,o in zip(self.obj, other.obj)) and \
+            any( s != o for s,o in zip(self.obj, other.obj)):
+            return True
+        return False
+
+def indiv_sort(population, key=-1):
+    popsize = len(population)
+    if popsize <= 1:
+        return population
+
+    pivot = population[0]
+    left = []
+    right = []
+    for i in range(1, popsize):
+        indiv = population[i]
+        # print(indiv.obj, indiv.obj[key], pivot.obj[key])
+        if indiv.obj[key] <= pivot.obj[key]:
+            left.append(indiv)
+        else:
+            right.append(indiv)
+    # print()
+    left = indiv_sort(left, key)
+    right = indiv_sort(right, key)
+    
+    center = [pivot]
+    return left+center+right
+
+        
+class NonDominatedSort(object):
+
+    def __init__(self):
+        pass
+        # self.pop = pop
+
+    def sort(self, population:list, return_rank=False):
+        popsize = len(population)
+
+        is_dominated = np.empty((popsize, popsize), dtype=np.bool)
+        num_dominated = np.zeros(popsize, dtype=np.int64)
+        mask = np.empty(popsize, dtype=np.bool)
+        rank = np.zeros(popsize, dtype=np.int64)
+
+        for i in range(popsize):
+            for j in range(popsize):
+                # if i == j:
+                #     continue
+                #iがjに優越されている -> True
+                dom = population[j].dominate(population[i])
+                is_dominated[i,j] = (i!= j) and dom
+
+        #iを優越する個体の数
+        is_dominated.sum(axis=(1,), out=num_dominated)
+        # print(num_dominated)
+
+        fronts = []
+        limit = popsize
+        for r in range(popsize):
+            front = []
+            for i in range(popsize):
+                is_rank_ditermined = not(rank[i] or num_dominated[i])
+                mask[i] = is_rank_ditermined
+                if is_rank_ditermined:
+                    rank[i] = r + 1
+                    front.append(population[i])
+                
+            fronts.append(front)
+            limit -= len(front)
+
+            if return_rank:
+                if rank.all():
+                    return rank 
+            elif limit <= 0:
+                return fronts
+
+            # print(np.sum(mask & is_dominated))
+            num_dominated -= np.sum(mask & is_dominated, axis=(1,))
+
+        raise Exception("Error: reached the end of function")
 
 class HyperVolume(object):
     def __init__(self, pareto):
         self.pareto = pareto
-        self.obj_dim = pareto.shape[-1]
+        self.pareto_sorted = indiv_sort(self.pareto)
+        self.ref_point = np.ones(pareto[0].n_obj)
+        self.set_refpoint()
+        self.obj_dim = pareto[0].n_obj
+        self.volume = 0
+
+    def set_refpoint(self, opt="minimize"):
+        pareto_arr = []
+        for indiv in self.pareto:
+            pareto_arr.append(indiv.obj)
+        pareto_arr = np.array(pareto_arr)
+
+        minmax = max 
+        if opt == "maximize":
+            minmax = min
+
+        for i in range(len(self.ref_point)):
+            self.ref_point[i] = minmax(pareto_arr[:,i])
+
+    def hso(self):
+        pl, s = self.obj_dim_sort(self.pareto)
+        s = [pl, s]
+        for k in range(self.obj_dim):
+            s_dash = []
+
 
     def calculate(self, obj_dim):
         pass
 
-    def slicing(self, pareto):
-        pareto_sorted = self.obj_dim_sort(pareto)
+    def calc_2d(self):
+        if len(self.ref_point) != 2:
+            return NotImplemented
+        
+        vol = 0
+        b_indiv = None
+        for i, indiv in enumerate(self.pareto_sorted):
+            if i == 0 or i == len(self.pareto_sorted):
+                b_indiv = indiv
+                continue
+
+            x = (b_indiv.obj[0] - indiv.obj[0])
+            y = (self.ref_point[1] - b_indiv.obj[1])
+            vol += x*y
+            b_indiv = indiv
+            print(f"vol:{vol:.10f}  x:{x:.5f}  y:{y:.5f}")
+        
+        self.volume = vol
+        return vol
         
 
-    def obj_dim_sort(self, pareto):
-        return pareto[pareto[:,-1].argsort(), :], 
+    def obj_dim_sort(self, dim=-1):
+        pareto_arr = []
+        for indiv in self.pareto:
+            pareto_arr.append(indiv.obj)
+        
+        pareto_arr = np.array(pareto_arr)
+        res_arr = pareto_arr[pareto_arr[:,dim].argsort(), :]
+        self.pareto_sorted = res_arr
+        
+        return res_arr, res_arr[:,dim]
+
+
+
+def indiv_plot(population:list, color=None):
+    evals = []
+    for indiv in (population):
+        # print(indiv)
+        evals.append(indiv.obj)
+    
+    evals = np.array(evals)
+
+    plt.scatter(evals[:,0], evals[:,1], c=color)
+    
 
 if __name__ == "__main__":
     front = np.array([[11,4,4],
@@ -21,9 +175,48 @@ if __name__ == "__main__":
                       [5,6,7],
                       [3,3,10]])
 
-    hypervol = HyperVolume(front)
-    front_sort = hypervol.obj_dim_sort(front)
 
-    print(front)
-    print()
-    print(front_sort)
+    front = np.array([[11,4],
+                      [9,2],
+                      [5,6],
+                      [3,3]])
+
+
+    dat = np.loadtxt("tablex.txt", skiprows=1)
+    sortfunc = NonDominatedSort()
+    population = []
+    for s in dat:
+        population.append(Individual(s[1:6], s[6:]))
+        # print(population[-1].__dict__)
+
+    front = sortfunc.sort(population)
+
+    # front_sort = hypervol.obj_dim_sort(front)
+
+    pareto = front[0]
+    print(len(pareto))
+    print("front::", pareto)
+    
+    hypervol = HyperVolume(pareto)
+
+    print("not sorted")
+    for indiv in pareto:
+        print(indiv.obj)
+    
+    print("sorting")
+    pareto = indiv_sort(pareto, key=0)
+    
+    print("sorted")
+    for indiv in pareto:
+        print(indiv.obj)
+    
+    vol = hypervol.calc_2d()
+    print("refpoint",hypervol.ref_point)
+    print(vol)
+
+    indiv_plot(population)
+    indiv_plot(pareto, color="Red")
+    plt.show()
+    # print()
+    # print(front_sort)
+
